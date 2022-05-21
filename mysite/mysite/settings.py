a572e35079e6ab62f182e69ef469500147b73040
Tanja <tanjang21@hotmail.com>
2022-05-21 16:13:53 +0200

    finish website

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,15 +1,16 @@
+from distutils.debug import DEBUG
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-DEBUG=True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#*oa0%mwj-v9kx7!q)525#%&q%=_2y5qcn__%5q7^k_o@b0f*j'
+
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
